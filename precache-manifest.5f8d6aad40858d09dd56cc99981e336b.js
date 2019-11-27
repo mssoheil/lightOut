62aa6c1719864cd84bdd00c4e0c436750a63986f
mssoheil88 <mssoheil88@yahoo.com>
2019-11-27 15:31:35 +0330

    Updates

---
+++ precache-manifest.5f8d6aad40858d09dd56cc99981e336b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84e0da81438b16839897da993806a0a",
+    "url": "/lightOut/index.html"
+  },
+  {
+    "revision": "3c4acd6bc5bbf2a94203",
+    "url": "/lightOut/static/css/main.43ad6bf3.chunk.css"
+  },
+  {
+    "revision": "92134196c49c54981954",
+    "url": "/lightOut/static/js/2.4b46ff3e.chunk.js"
+  },
+  {
+    "revision": "3c4acd6bc5bbf2a94203",
+    "url": "/lightOut/static/js/main.2252ddb5.chunk.js"
+  },
+  {
+    "revision": "2fd1fdcedad09d8730de",
+    "url": "/lightOut/static/js/runtime-main.dd476acc.js"
+  }
+]);
